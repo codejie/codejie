050f75ec12033af9a4250eba96cb3c23f38f392f
codejie@gmail.com <codejie@gmail.com@3708cebf-c5f0-0022-90b4-431c9d269242>
2012-02-08 09:32:13 +0000

    MoonEclipse with ad

--- java/gdx/MoonEclipse-Android/src/jie/java/android/gdx/mooneclipse/MoonEclipse.java
+++ java/gdx/MoonEclipse-Android/src/jie/java/android/gdx/mooneclipse/MoonEclipse.java
@@ -4,13 +4,38 @@ package jie.java.android.gdx.mooneclipse;
 import com.badlogic.gdx.backends.android.AndroidApplication;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.RelativeLayout;
 
 public class MoonEclipse extends AndroidApplication {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //initialize(new MoonEclipseGame(), false);
         //setContentView(R.layout.main);
-        initialize(new MoonEclipseGame(), false);
-    }    
+        
+        //http://code.google.com/p/libgdx/wiki/AdMobInLibgdx
+        RelativeLayout layout = new RelativeLayout(this);
+        
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        this.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
+        
+        View gameView = this.initializeForView(new MoonEclipseGame(), false);
+        layout.addView(gameView);
+
+        initAdView(layout);
+        
+        this.setContentView(layout);        
+    }
+
+	private void initAdView(RelativeLayout layout) {
+		// TODO Auto-generated method stub
+		
+	}
+    
+    
 }

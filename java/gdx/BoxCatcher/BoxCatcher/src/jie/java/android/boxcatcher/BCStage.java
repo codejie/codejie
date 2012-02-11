c42bb0e76e37899ff995d53a5c7d563ca07325a8
codejie@gmail.com <codejie@gmail.com@3708cebf-c5f0-0022-90b4-431c9d269242>
2012-02-11 14:29:59 +0000

    BoxCatcher - via MacBook Pro

--- java/gdx/BoxCatcher/BoxCatcher/src/jie/java/android/boxcatcher/BCStage.java
+++ java/gdx/BoxCatcher/BoxCatcher/src/jie/java/android/boxcatcher/BCStage.java
@@ -1,8 +1,33 @@
 package jie.java.android.boxcatcher;
 
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.scenes.scene2d.Group;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 
-public class BCStage extends Group {	
+public class BCStage extends Group implements InputProcessor {
+
+	@Override
+	public boolean touchDown(int x, int y, int pointer, int button) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchUp(int x, int y, int pointer, int button) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchDragged(int x, int y, int pointer) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchMoved(int x, int y) {
+		// TODO Auto-generated method stub
+		return false;
+	}	
 
 }
--- java/gdx/BoxCatcher/BoxCatcher/src/jie/java/android/boxcatcher/TestScreen.java
+++ java/gdx/BoxCatcher/BoxCatcher/src/jie/java/android/boxcatcher/TestScreen.java
@@ -2,18 +2,42 @@ package jie.java.android.boxcatcher;
 
 public class TestScreen extends BCScreen {
 
-	// TestStage stage = null;
+	TestStage stage = null;
 	
 	public TestScreen(BCGame game) {
 		super(game);
 		
-		//stage = new TestStage();
+		stage = new TestStage();
 	}
 
 	@Override
 	public void show() {
-		this.addActor(new TestStage());
+		this.addActor(stage);
 		super.show();
+	}
+
+	@Override
+	public boolean touchDown(int x, int y, int pointer, int button) {
+		// TODO Auto-generated method stub
+		return super.touchDown(x, y, pointer, button);
+	}
+
+	@Override
+	public boolean touchUp(int x, int y, int pointer, int button) {
+		// TODO Auto-generated method stub
+		return super.touchUp(x, y, pointer, button);
+	}
+
+	@Override
+	public boolean touchDragged(int x, int y, int pointer) {
+		// TODO Auto-generated method stub
+		return super.touchDragged(x, y, pointer);
+	}
+
+	@Override
+	public boolean touchMoved(int x, int y) {
+		// TODO Auto-generated method stub
+		return super.touchMoved(x, y);
 	}
 
 	@Override

bfde0ae550a960c239d04a34817a2f0f690fa2d6
codejie@gmail.com <codejie@gmail.com@3708cebf-c5f0-0022-90b4-431c9d269242>
2012-03-05 16:24:32 +0000

    BoxCatcher - via MacBook Pro

--- java/gdx/BoxCatcher/BoxCatcher/src/jie/java/android/boxcatcher/StageData.java
+++ java/gdx/BoxCatcher/BoxCatcher/src/jie/java/android/boxcatcher/StageData.java
@@ -1,26 +1,72 @@
 package jie.java.android.boxcatcher;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import jie.java.android.boxcatcher.BoxActor.BoxShape;
+
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
 
 public class StageData {
 
-	public class Screen {
+	public final static class Screen {
 		public int width	=	0;
 		public int height	=	0;
 	}
 	
-	public class World {
+	public final static class World {
 		//public float scale		=	1.0f;
 		public Vector2 gravity	=	new Vector2(0, 0);
 	}
 	
-	public class Box {
-		public BoxActor.Parameter	=	null;
-		// Texture?
+	public final static class Box {
+		public String name;
+		public BodyType type;
+		public BoxShape shape;
+		public float x, y;
+		public float width, height;
+		public float angle = 0.0f;
+		public float density = 1.0f;
+		public float restitution = 0.0f;
+		public float friction = 0.0f;
+		public short filterBits = 0x00FF;
+		
+		//Texture
+		public TextureRegion texture = null;
+		public Animation animation = null;
+		
+		//Sound
 	}
 	
-	public class Boxes {
-		
+	public Screen screen = null;
+	public World world = null;
+	public HashMap<Float, ArrayList<Box>> boxes = new HashMap<Float, ArrayList<Box>>();
+	
+	private int stageID = -1;
+	private float deltaStep = 0.0f;
+	
+	public StageData(int id) {
+		stageID = id;
+	}
+
+	@Override
+	protected void finalize() throws Throwable {
+		super.finalize();
+	}
+	
+	public int load() {
+		return -1;
+	}
+	
+	public Box getNextBox(float delta) {
+		return null;
+	}
+	
+	public TextureRegion getNextRegion(float delta) {
+		return null;
 	}
 	
 }
---
+++ java/gdx/BoxCatcher/BoxCatcher/src/jie/java/android/boxcatcher/TextureData.java
@@ -0,0 +1,11 @@
+package jie.java.android.boxcatcher;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+
+public class TextureData {
+
+	public Texture[] texture = null;
+	public Animation[] animation = null;
+	
+}

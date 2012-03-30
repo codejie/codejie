388ba38f4e62a2e0877aaaca053b97063550cfde
codejie@gmail.com <codejie@gmail.com@3708cebf-c5f0-0022-90b4-431c9d269242>
2012-03-30 09:31:05 +0000

    BoxCatcher

--- java/gdx/BoxCatcher/BoxCatcher/src/jie/java/android/boxcatcher/DBAccess.java
+++ java/gdx/BoxCatcher/BoxCatcher/src/jie/java/android/boxcatcher/DBAccess.java
@@ -10,8 +10,23 @@ public class DBAccess {
 	private final String DATABASE_FILE	=	"db.db";
 	
 	private final String TABLE_NAME_BOXES		=	"Boxes";
+	private final String TABLE_NAME_STAGES		=	"Stages";
+	private final String TABLE_NAME_STAGEBOX	=	"StageBox";
 	
-	private final String TABLE_COLUMN_INDEX		=	"id";
+	private final String TABLE_COLUMN_INDEX		=	"idx";
+	private final String TABLE_COLUMN_NAME		=	"name";
+	private final String TABLE_COLUMN_RACE		=	"race";
+	private final String TABLE_COLUMN_TYPE		=	"type";
+	private final String TABLE_COLUMN_SHAPE		=	"shape";
+	private final String TABLE_COLUMN_X			=	"x";
+	private final String TABLE_COLUMN_Y			=	"y";
+	private final String TABLE_COLUMN_WIDTH		=	"width";
+	private final String TABLE_COLUMN_HEIGHT	=	"height";
+	private final String TABLE_COLUMN_ANGLE		=	"angle";
+	private final String TABLE_COLUMN_DENSITY	=	"density";
+	private final String TABLE_COLUMN_RESTITUTION	=	"resti";
+	private final String TABLE_COLUMN_FRICTION		=	"fric";
+	private final String TABLE_COLUMN_FILTERBITS	=	"filter";
 	
 	private Connection conn = null;
 	
@@ -24,7 +39,7 @@ public class DBAccess {
 			
 			createDatabase(DATABASE_FILE);
 			
-			createTable(TABLE_NAME_BOXES);
+			createTables();
 		}
 		catch (SQLException e) {
 			Gdx.app.log("tag", "dbaccess sql exception - " + e.toString());
@@ -51,12 +66,34 @@ public class DBAccess {
 		conn = DriverManager.getConnection("jdbc:sqlite:" + file);
 	}	
 	
-	private void createTable(String name) throws SQLException {
-		Statement stat = conn.createStatement();
-		stat.execute("CREATE TABLE IF NOT EXISTS " + name + " ("
-				+ TABLE_COLUMN_INDEX + " INTEGER PRIMARY KEY"
-				+ ");");
+	private void createTables() throws SQLException {
+		
+		String sql = "CREATE TABLE IF NOT EXISTS " + TABLE_NAME_BOXES + " ("
+				+ TABLE_COLUMN_INDEX + " INTEGER PRIMARY KEY,"
+				+ TABLE_COLUMN_NAME + " TEXT," 
+				+ TABLE_COLUMN_RACE + " INTEGER,"
+				+ TABLE_COLUMN_TYPE + " INTEGER,"
+				+ TABLE_COLUMN_SHAPE + " INTEGER,"
+				+ TABLE_COLUMN_X + " INTEGER,"
+				+ TABLE_COLUMN_Y + " INTEGER,"
+				+ TABLE_COLUMN_WIDTH + " INTEGER,"
+				+ TABLE_COLUMN_HEIGHT + " INTEGER,"
+				+ TABLE_COLUMN_ANGLE + " REAL,"
+				+ TABLE_COLUMN_DENSITY + " REAL,"
+				+ TABLE_COLUMN_RESTITUTION + " REAL,"
+				+ TABLE_COLUMN_FRICTION + " REAL,"
+				+ TABLE_COLUMN_FILTERBITS + " INTEGER"
+				+ ");";
 
+		execSQL(sql);
+		
+		//sql = 
+		
+	}
+
+	private void execSQL(String sql) throws SQLException {
+        Statement stat = conn.createStatement();
+        stat.execute(sql);
 	}
 
 

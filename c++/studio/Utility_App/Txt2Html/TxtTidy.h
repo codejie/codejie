2c61f5bda2fe27afaca2674676fa08725513c41d
codejie@gmail.com <codejie@gmail.com@3708cebf-c5f0-0022-90b4-431c9d269242>
2012-07-26 08:21:01 +0000

    Move Txt2Html to Utility_App

---
+++ c++/studio/Utility_App/Txt2Html/TxtTidy.h
@@ -0,0 +1,34 @@
+#ifndef __TXTTIDY_H__
+#define __TXTTIDY_H__
+
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+class TxtTidy
+{
+public:
+	static const char SEPARATOR	=	'|';
+public:
+	struct TData
+	{
+		std::string word;
+		std::string symbol;
+		std::vector<std::pair<std::string, std::string> > data;
+	};
+	
+public:
+	TxtTidy() {}
+	virtual ~TxtTidy() {}
+
+//	int Load(const std::string& file);
+	int Tidy(const std::string& input, const std::string& output);
+private:
+	int AnalyseData(std::ofstream& ofs, const std::string& data) const;
+	int AnalyseSubData(std::ofstream& ofs, const std::string& data) const;
+private:
+	std::ifstream _ifs;
+};
+
+#endif

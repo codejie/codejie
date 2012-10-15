dadfbdc400e912160b6a5575b6ebd4f5a90e55dc
codejie@gmail.com <codejie@gmail.com@3708cebf-c5f0-0022-90b4-431c9d269242>
2012-10-15 11:48:33 +0000

    Test - LD2 decode test from Lingoes-Extractor project

---
+++ temp_proj/java/LD2_Test/src/cn/kk/extractor/lingoes/Main.java
@@ -0,0 +1,46 @@
+package cn.kk.extractor.lingoes;
+
+import java.io.File;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Main m = new Main();
+		m.run();
+		
+	}
+	
+	private void run() {
+		
+		final String input = "./data/3GPP.ld2";
+		final String output = "./data/3GPP.txt";
+		
+		try {
+			
+			LingoesLd2Extractor extractor = new LingoesLd2Extractor(this);
+			
+			extractor.extractLd2ToFile(new File(input), new File(output));
+			
+		} catch (final Throwable t) {
+			
+		}		
+	}
+
+	public void setStatusDirect(int counter, int counter2, int calcSpeed) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setStatus(int defTotal, int i, int j) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setExample(int i, String cut) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+}

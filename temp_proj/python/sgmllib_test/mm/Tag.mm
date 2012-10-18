5a94f5f61d908411a433ae5372eac45525173b15
codejie@gmail.com <codejie@gmail.com@3708cebf-c5f0-0022-90b4-431c9d269242>
2012-10-18 10:07:34 +0000

    Test - sgmllib_test : analyze tag

---
+++ temp_proj/python/sgmllib_test/mm/Tag.mm
@@ -0,0 +1,71 @@
+<map version="freeplane 1.2.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Tag" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1350554126898"><hook NAME="MapStyle">
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="1"/>
+<node TEXT="C" POSITION="right" ID="ID_1176441314" CREATED="1350554130547" MODIFIED="1350554322455" HGAP="40" VSHIFT="-10">
+<edge COLOR="#ff0000" WIDTH="3"/>
+<node TEXT="E - Extend" ID="ID_1838068692" CREATED="1350554207317" MODIFIED="1350554290477"/>
+<node TEXT="F - Block" ID="ID_1908519341" CREATED="1350554330829" MODIFIED="1350554407380">
+<node TEXT="H" ID="ID_746312277" CREATED="1350554423100" MODIFIED="1350554433184">
+<node TEXT="M - Symbol" ID="ID_67927182" CREATED="1350554436757" MODIFIED="1350554444552"/>
+</node>
+<node TEXT="I - Meaning" ID="ID_1801378991" CREATED="1350554473039" MODIFIED="1350554482057">
+<node TEXT="N - Meaning" ID="ID_941838134" CREATED="1350554483196" MODIFIED="1350554496857">
+<node TEXT="U - Category" ID="ID_1744042271" CREATED="1350554496872" MODIFIED="1350554525529">
+<edge COLOR="#ff0000"/>
+</node>
+<node TEXT="Meaning" ID="ID_1348651009" CREATED="1350554565867" MODIFIED="1350554571981"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>

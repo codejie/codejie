a07686f293774de77e0c53576a8aa5c97cf7a636
codejie@gmail.com <codejie@gmail.com@3708cebf-c5f0-0022-90b4-431c9d269242>
2012-10-22 10:07:40 +0000

    Test - sgmllib_test : analyze tag

--- temp_proj/python/sgmllib_test/src/htmlparser.py
+++ temp_proj/python/sgmllib_test/src/htmlparser.py
@@ -41,14 +41,14 @@ class MyParser(HTMLParser):
         elif self.pos == 5:
             self.pos = 2
         elif self.pos == 2:
-            self.levelField -= 1
+#            self.levelField -= 1
             self.pos = -1
         elif self.pos == 6:
             self.pos = 5
         elif self.pos == 7:
             self.pos = 6
         elif self.pos == 5:
-            self.levelMeaning -= 1
+#            self.levelMeaning -= 1
             self.pos = 2
         else:
             self.pos = -1
@@ -58,7 +58,7 @@ class MyParser(HTMLParser):
             self.result.extend = data
         elif self.pos == 4:
             self.result.field[self.levelField].symbol = data
-        elif self.pos == 5:
+        elif self.pos == 7:
             self.result.field[self.levelField].meaning[self.levelMeaning].category = data
         elif self.pos == 6:
             self.result.field[self.levelField].meaning[self.levelMeaning].meaning = data
@@ -74,14 +74,14 @@ class DictMeaning:
     meaning = ''
     
     def __str__(self):
-        return 'category = %s meaning = %s' % (self.meaning, self.meaning)
+        return '[category = %s meaning = %s]' % (self.category, self.meaning)
 
 class DictField:
     symbol = ''
     meaning = []
     
     def __str__(self):
-        return 'symbol = %s meaning = %s' % (self.symbol, string.join(map(str, self.meaning)))
+        return '[symbol = |%s| meaning = %s]' % (self.symbol, string.join(map(str, self.meaning)))
 
 class DictData:
     word = ''
@@ -107,7 +107,10 @@ def analyseLine(str, output):
     parseHtml(html, output)
 
 def main():
-    str = 'abandon = <C><E>abandons|abandoned|abandoning</E><F><H><M>a·ban·don || ə\'bændən</M></H><I><N><U>n.</U>  放纵, 放任; 狂热</N></I><I><N><U>v.</U>  丢弃; 中止, 放弃; 遗弃, 抛弃; 使放纵</N></I></F></C>'
+#    str = 'abandon = <C><E>abandons|abandoned|abandoning</E><F><H><M>a·ban·don || ə\'bændən</M></H><I><N><U>n.</U>  放纵, 放任; 狂热</N></I><I><N><U>v.</U>  丢弃; 中止, 放弃; 遗弃, 抛弃; 使放纵</N></I></F></C>'
+#    str = 'a = <C><F><H><M>e?;?</M></H><I><N>字母A</N></I></F></C>'
+    str = 'AAA = <C><F><H><L>AAA (American Automobile Association)</L></H><I><N>美国汽车协会, 给在会会员提供紧急通路服务 (牵引等) 和其它汽车服务 (地图, 导游等) 的组织</N></I></F><E>A A A</E><F><H><L>AAA (antiaircraft artillery)</L></H><I><N>对空作战方法 (使用大炮或导弹)</N></I></F><E>A A A</E><F><H><L>AAA (Amateur Athletic Association)</L></H><I><N>业余体育协会, 监督非职业体育活动的组织</N></I></F></C>'
+#    str = 'AB = <C><F><H><L>AB (air base)</L></H><I><N>ab, 空军基地, 空军的军事基地, 空军训练和操作的中心之一</N></I></F></C>'
     data = DictData()
     analyseLine(str, data)
 

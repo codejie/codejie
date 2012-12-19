6e0f7c21e199cb16aa0c972b4ab5c56db7d9791c
codejie@gmail.com <codejie@gmail.com@3708cebf-c5f0-0022-90b4-431c9d269242>
2012-12-19 10:14:31 +0000

    Demo Dictionary of LAC2 - add dictionary activity

---
+++ java/DemoDictionaryOfLAC2/src/jie/java/android/demodictionaryoflac2/DictionaryActivity.java
@@ -0,0 +1,15 @@
+package jie.java.android.demodictionaryoflac2;
+
+import android.app.Activity;
+
+public class DictionaryActivity extends Activity {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- java/DemoDictionaryOfLAC2/src/jie/java/android/demodictionaryoflac2/MainActivity.java
+++ java/DemoDictionaryOfLAC2/src/jie/java/android/demodictionaryoflac2/MainActivity.java
@@ -3,20 +3,45 @@ package jie.java.android.demodictionaryoflac2;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnTouchListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        if(check() != 0) {
+        	this.finish();
+        }
     }
 
-    @Override
+    private int check() {  
+		return 0;
+	}
+
+	@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
     }
+
+	@Override
+	public boolean onTouch(View view, MotionEvent event) {
+
+		if(event.getAction() == MotionEvent.ACTION_UP) {
+			showDictionaryActivity();
+		}
+		
+		return true;
+	}
+
+	private void showDictionaryActivity() {
+
+	}
     
 }
